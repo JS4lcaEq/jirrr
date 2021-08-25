05e58d6bcd291052ed7c28a81f25b49031b5de21
condorrr <condorrr@gmail.com>
2021-08-25 17:56:29 +0300

    ini

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,285 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <el-col :span="2" class="desc">
+        общие трудозатраты:
+      </el-col>    
+      <el-col :span="1">
+      {{tasksCount}} 
+      </el-col>  
+      <el-col :span="2" class="desc">
+        разработчик:
+      </el-col> 
+      <el-col :span="3">
+        <el-input v-model="cMember.name"></el-input>
+      </el-col>
+
+       <el-col :span="1" class="desc">
+        <el-button @click="onDel">Удалить</el-button>
+      </el-col> 
+      <el-col :span="2">
+
+      </el-col>     
+
+
+    </el-row>
+
+    <el-row class="line">
+      <el-col :span="24" class="desc">
+        <hr />
+      </el-col> 
+    </el-row>
+      
+    <el-row>
+      <el-col :span="5" class="desc">
+        задача:
+      </el-col>      
+      <el-col :span="3">
+        <el-input v-model="cTask.name"></el-input>
+      </el-col>
+      <el-col :span="2" class="desc">
+        затраты:
+      </el-col>       
+
+      <el-col :span="1">
+        <el-input v-model="cTask.time"></el-input>
+      </el-col>
+       <el-col :span="2">
+        <el-button @click="onDelTask">Удалить</el-button>
+      </el-col>     
+    </el-row>
+    <el-row class="line">
+      <el-col :span="24" class="desc">
+        <hr />
+      </el-col> 
+    </el-row>
+  <div v-for="item in members" :key="item.id" class="member" :class="{'active': item.isActive}" @click="onSelect(item)">
+    <div class="nm">{{item.name}}</div>
+    <div  class="task" 
+          v-for="task in item.tasks" 
+          :key="task.id" 
+          :class="{'active': task.isActive, 'w2': task.time==2, 'w3': task.time==3, 'w4': task.time==4, 'w5': task.time==5}" 
+          @click="onSelectTask(task)">
+          <div>
+            {{task.name}}
+          </div>
+    </div>
+    <el-button @click="onAddTask" v-if="item.isActive">+</el-button>
+  </div>
+
+
+<div class="space"></div>
+  <el-button @click="onAdd">+</el-button>
+<div class="space"></div>
+
+    <canvas id="fire" width="500" height="500"></canvas>
+    <div class="debug">
+    <p>{{members}}</p>  
+    <p>{{fire}}</p>
+
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+let element
+let	context
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data(){
+    return{
+      members: [ { "id": 0.11482149135454323, "name": "Максим", "tasks": [ { "id": 0.0023599485286690536, "name": "новая задача", "time": "2", "isActive": false }, { "id": 0.37211958881958207, "name": "новая задача", "time": "2", "isActive": false }, { "id": 0.9343021127754118, "name": "новая задача", "time": "2", "isActive": false }, { "id": 0.18407621000603458, "name": "новая задача", "time": "2", "isActive": false }, { "id": 0.43270851236231545, "name": "новая задача", "time": "2", "isActive": false } ], "isActive": false }, { "id": 0.29428513232894016, "name": "Катя", "tasks": [ { "id": 0.9271747778856098, "name": "новая задача", "time": "5", "isActive": true }, { "id": 0.2861063346717799, "name": "новая задача", "time": "5", "isActive": false } ], "isActive": true }, { "id": 0.22876539886181424, "name": "Андрей", "tasks": [ { "id": 0.0065651665361503575, "name": "325 пейджер", "time": "3", "isActive": false }, { "id": 0.4716512229226457, "name": "283 флекс", "time": "5", "isActive": false }, { "id": 0.07985047626073227, "name": "326 признак архив", "time": "2", "isActive": false } ], "isActive": false } ] ,
+      inMember: "inMember",
+      cMember: { id: 1, name: "", tasks:[] },
+      tasks: [],
+      cTask:{}
+    }
+  },
+  mounted(){
+    element = document.getElementById("fire")
+    context = element.getContext('2d')
+
+
+  },
+  computed:{
+    tasksCount(){
+      let count = 0
+      this.members.forEach((member)=>{
+        member.tasks.forEach((task)=>{
+          count = Number.parseInt(count) + Number.parseInt(task.time)
+        })
+      })
+      return count
+    },
+    kH(){
+      return 500 / this.tasksCount
+    },
+    kW(){
+      return 50
+    },
+    fire(){
+      let ret = {}
+      this.members.forEach((member)=>{
+        let start = 0
+        member.tasks.forEach((task)=>{
+          let index = Number.parseInt(start) + Number.parseInt(task.time)
+          if(ret[index]== undefined){
+            ret[index] = 0
+          }
+          ret[index] = ret[index] + Number.parseInt(task.time)
+          start = index
+        })
+      })
+      if(context){
+        context.clearRect(0, 0, 500, 500);
+        context.beginPath()
+        context.moveTo(1, 1) 
+        let summ = 0 
+        for (var key = 0; key < 11; key++) {
+          if(ret[key.toString()]){
+            summ = summ + ret[key.toString()]
+          }
+          context.lineTo(key * this.kW, summ * this.kH)
+          console.log(key, "=", ret[key.toString()])
+        } 
+        context.strokeStyle = "#000";     
+        context.stroke();
+        context.closePath()
+        context.beginPath()
+        for(let i = 0 ; i < 10; i++){
+          context.moveTo(i*50, 1)
+          context.lineTo(i*50, 500)        
+        }
+        for(let i = 0 ; i < this.tasksCount; i++){
+          context.moveTo(1, i*this.kH)
+          context.lineTo(500, i*this.kH)        
+        }
+        context.strokeStyle = 'rgba(0, 0, 0, 0.1)';
+        context.stroke();
+      }
+
+      return ret
+    }
+  },
+  methods:{
+    onSave(){
+      // let self = this;
+      // this.dt.push({idname: self.inMember, tasks:[]})
+    },
+    onDel(){
+      let indexToDel = this.members.indexOf(this.cMember)
+      this.members.splice(indexToDel, 1)
+      this.cMember = {}
+      this.cTask = {}
+    },
+    onDelTask(){
+      let index = 0
+      let indexToDel
+      this.cMember.tasks.forEach((task)=>{
+        if(task.id==this.cTask.id){
+          indexToDel = index
+        }
+        index++
+      })
+      console.log("indexToDel", indexToDel)
+      this.cMember.tasks.splice(indexToDel, 1)
+      this.cTask = {}
+    },
+    onAdd(){
+      let item = {id: null, name: "новый участник", tasks:[], isActive: false}
+      this.members.push(item)
+      item.id = Math.random()
+    },
+    onSelect(member){
+      this.cMember = member;
+      this.clearMembersIsActive();
+      member.isActive = true;
+    },
+    onAddTask(){
+      let self = this;
+      let task = {id: Math.random(), name: "новая задача", time: 1, isActive: false}
+      //self.tasks.push(task)
+      self.cMember.tasks.push(task)
+    },
+    clearMembersIsActive(){
+      this.members.forEach((member)=>{member.isActive = false})
+    },
+
+    onSelectTask(task){
+      this.cTask = task
+      this.members.forEach((member)=>{
+        member.tasks.forEach((tsk)=>{tsk.isActive = false})
+      })
+      task.isActive = true;
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.el-row.line hr { 
+  color: #eee;
+}
+.debug{
+  color: #eee;
+}
+.member{
+background-color:#eee;
+margin: 10px;
+height: 40px;
+border-bottom: solid 2px #fff;
+}
+.member.active{
+  border-bottom: solid 2px red;
+}
+.task{
+width: 100px;
+float: left;
+border-top: solid 2px #fff;
+height: 35px;
+
+}
+.task div{
+  background-color: aquamarine;
+  margin: 0 3px;
+  height: 100%;
+}
+.task.active{
+  border-top: solid 2px red;
+}
+.task.w2{
+  width: 200px;
+}
+.task.w3{
+  width: 300px;
+}
+.task.w4{
+  width: 400px;
+}
+.task.w5{
+  width: 500px;
+}
+.nm{
+  width: 200px;
+  float: left;
+  } 
+.space{
+  clear: both;
+  width: 100%;
+}
+
+#fire{
+  border: solid 1px #ccc;
+}
+.desc{
+  text-align: right;
 }
 </style>
